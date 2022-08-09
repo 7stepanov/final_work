315cf138abb1b612800a25db6d048d2a86fac35d
Aleksey <stepanovaleks194@gmail.com>
2022-08-09 20:59:58 +0500

    Выполнил 1-е задание к итоговой контрольной

--- exx001/Program.cs
+++ exx001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,длина которых меньше либо равна 3. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+void getArray (string [] array)
+{
+    string [] myArr = new string [array.Length];
+    for(int i = 0; i < array.Length; i++ )
+    {
+        if (array[i]. Length <= 3 ){
+        myArr[i] = array[i];
+        Console.WriteLine(myArr[i]);
+        }
+    }
+    Console.WriteLine();
+}
+void PrintArray(string [] Array){
+    for (int i = 0; i < Array.Length; i++){
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+string [] array = {"1234", "1567", "-2", "computer science"};
+
+getArray(array);
+PrintArray(array);
